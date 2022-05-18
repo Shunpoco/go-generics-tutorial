f6f63f74953f8bbaeb24ec032c6ff07211e8575d
Shunpoco <tkngsnsk313320@gmail.com>
2022-05-18 21:58:45 +0900

    Generics

--- main.go
+++ main.go
@@ -22,6 +22,19 @@ func SumFloats(m map[string]float64) float64 {
 	return s
 }
 
+// SumIntsOrFloats sums the values of map m. It supports both Int64 and float64
+// as types for map values.
+
+// Go requires that map keys be comparable
+func SumIntsOrFloats[k comparable, V int64 | float64](m map[k]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+
+	return s
+}
+
 func main() {
 	ints := map[string]int64{
 		"first":  34,
@@ -36,4 +49,22 @@ func main() {
 		SumInts(ints),
 		SumFloats(floats),
 	)
+
+	fmt.Printf("Generic Sums: %v and %v\n",
+		SumIntsOrFloats[string, int64](ints),
+		SumIntsOrFloats[string, float64](floats),
+	)
+
+	// We also ommit type specifications
+	fmt.Printf("Generics Sums wo type specifications: %v and %v\n",
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats),
+	)
+
+	// Because K is comparable, we can also use integer
+	intsWithInt := map[int]int64{
+		1: 10,
+		2: 20,
+	}
+	fmt.Println(SumIntsOrFloats[int, int64](intsWithInt))
 }
